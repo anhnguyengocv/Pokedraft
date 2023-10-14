8cee0d889e294bcf48b1c76ac0e18c055f3632d6
anhnguyengocv <anhnguyengocv@gmail.com>
2023-10-14 18:38:01 -0500

    Updated scripts.js
    three pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
-alert("Hello world");
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
-favoriteFood = ' pasta';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Pichu',
+        height: 0.2,
+        types: 'electric'
+    },
+
+    {
+        name: 'Gastly',
+        height: 1.3,
+        types: ['ghost', 'poison']
+    },
+
+    {
+        name: 'Ralts',
+        height: 0.4,
+        types: ['psychic', 'fairy']
+    }
+];
+
+
+//alert("Hello world");
+//let favoriteFood = 'sushi';
+//document.write(favoriteFood);
+//favoriteFood = ' pasta';
+//document.write(favoriteFood);
