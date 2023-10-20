4ee4b99489876efdd691f93c4ad6852bb65af584
anhnguyengocv <anhnguyengocv@gmail.com>
2023-10-19 19:53:56 -0500

    updated for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
 let pokemonList = [
-    {
-        name: 'Pichu',
-        height: 0.2,
-        types: ['electric']
+    { 
+        name: 'Pichu', 
+        height: 0.2, 
+        types: ['electric'] 
     },
-
-    {
-        name: 'Gastly',
-        height: 1.3,
-        types: ['ghost', 'poison']
+    { 
+        name: 'Gastly', 
+        height: 1.3, 
+        types: ['ghost', 'poison'] 
     },
-
-    {
-        name: 'Ralts',
-        height: 0.4,
-        types: ['psychic', 'fairy']
-    }
+    { 
+        name: 'Ralts', 
+        height: 0.4, 
+        types: ['psychic', 'fairy'] } 
 ];
 
+for (let i=0; i < pokemonList.length; i++) {
+    text = " " + pokemonList[i].name + " " + "(" + "height: " + pokemonList[i].height + ")";
+    document.write(text);
+    if (pokemonList[i].height > 1.0) {
+        document.write( " - " + pokemonList[i].name + " is so big!")
+    }
+}
 
 //alert("Hello world");
 //let favoriteFood = 'sushi';
