254fadf0c5ea60000f0f29142845ccde4cab3d9f
agnesnn123 <anhnguyengocv@gmail.com>
2023-10-25 14:59:39 -0500

    Updated scripts.js
    forEach
    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,45 @@
-let pokemonList = [
-    {
-      name: 'Pichu',
-      height: 0.2,
-      types: ['electric']
-    },
-    {
-      name: 'Gastly',
-      height: 1.3,
-      types: ['ghost', 'poison']
-    },
-    {
-      name: 'Ralts',
-      height: 0.4,
-      types: ['psychic', 'fairy']
-    }
-  ];
+let pokemonRepository = (function() {
+    let pokemonList = [
+      {
+        name: 'Pichu',
+        height: 0.2,
+        types: ['electric']
+      },
+      {
+        name: 'Gastly',
+        height: 1.3,
+        types: ['ghost', 'poison']
+      },
+      {
+        name: 'Ralts',
+        height: 0.4,
+        types: ['psychic', 'fairy']
+      }
+    ];
   
-  pokemonList.forEach(function(user) {
-    document.write("<p>" + user.name + " (" + "height: " + user.height + ")");"</p>"
-        if (user.height > 1.0) {
-        document.write(" - Wow, that's big!");
-        }
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i=0; i < pokemonList.length; i++) {
-//     text = "<p>" + pokemonList[i].name + " " + "(" + "height: " + pokemonList[i].height + ")";
-//     document.write(text);
-//     if (pokemonList[i].height > 1.0) {
-//         document.write( " - " + "Wow, that's big!")
-//     }
-// }
-//alert("Hello world");
-//let favoriteFood = 'sushi';
-//document.write(favoriteFood);
-//favoriteFood = ' pasta';
-//document.write(favoriteFood);
+    console.log(pokemonList);
+  
+    return {
+      getAll: function() {
+        return pokemonList;
+      },
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      }
+    };
+})();
+  
+  
+    function displayPokemonList() {
+        let pokemonList = pokemonRepository.getAll();
+        let pokemonListElement = document.getElementById('pokemon-list');
+  
+        pokemonList.forEach(function(pokemon) {
+            document.write("<p>" + pokemon.name + " (" + "height: " + pokemon.height + ")"); "</p>"
+            if (pokemon.height > 1.0) {
+                document.write(" - Wow, that's big!");
+            }
+        })();
+    }
+  
+  displayPokemonList();
