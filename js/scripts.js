2715242c6e9f7bceff7a1337fc4d83136b366a1d
agnesnn123 <anhnguyengocv@gmail.com>
2023-10-25 16:29:15 -0500

    Updated scripts.js
    filter function updated

--- js/scripts.js
+++ js/scripts.js
@@ -18,27 +18,62 @@ let pokemonRepository = (function() {
     ];
   
     console.log(pokemonList);
-  
+
+    //Functino to add a pokemon to the list
+    function add(pokemon){
+      if(typeof pokemon === 'object' && 'name' in pokemon) {
+    pokemonList.push(pokemon);
+    } else {
+      console.error('Invalid pokemon.');
+    }
+    }
+
+    //Function to gett all pokemon
+    function getAll() {
+      return pokemonList;
+    }
+
+    //Function to find pokemon by name using filter
+    function findByName(name) {
+      return pokemonList.filter(function(pokemon) {
+        return pokemon.name === name;
+      });
+    }
+
+    //Return the functinos and data you want to be accessible
     return {
-      getAll: function() {
-        return pokemonList;
-      },
-      add: function(pokemon) {
-        pokemonList.push(pokemon);
-      }
+      add: add,
+      getAll: getAll,
+      findByName: findByName
     };
+  
+    // return {
+    //   getAll: function() {
+    //     return pokemonList;
+    //   },
+    //   add: function(pokemon) {
+    //     pokemonList.push(pokemon);
+    //   }
+    // };
 })();
   
+//Example display
+let foundPokemon = pokemonRepository.findByName('Pikachu');
+if (foundPokemon.length > 0) {
+  console.log('Found Pokemon: ', foundPokemon);
+} else {
+  console.log('Pokemon not found.');
+}
   
-    function displayPokemonList() {
-      let pokemonList = pokemonRepository.getAll();
-  
-      pokemonList.forEach(function(pokemon) {
-        document.write("<p>" + pokemon.name + " (" + "height: " + pokemon.height + ")"); "</p>"
-        if (pokemon.height > 1.0) {
-            document.write(" - Wow, that's big!");
-        }
-      })();
+function displayPokemonList() {
+  let pokemonList = pokemonRepository.getAll();
+
+  pokemonList.forEach(function(pokemon) {
+    document.write("<p>" + pokemon.name + " (" + "height: " + pokemon.height + ")"); "</p>"
+    if (pokemon.height > 1.0) {
+        document.write(" - Wow, that's big!");
     }
+  })();
+}
   
 displayPokemonList();
